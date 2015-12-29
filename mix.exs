96bf92bdc0857bc3686a3cc6d4ebac2f7669d9b0
Eric Sullivan <eric.sullivan@annkissam.com>
2015-12-29 11:02:17 -0500

    relax mariaex and postrex requirements

--- mix.exs
+++ mix.exs
@@ -30,8 +30,8 @@ defmodule Ectophile.Mixfile do
 
   defp deps do
     [{:ecto, "~> 1.0"},
-     {:postgrex, "~> 0.9.1", optional: true},
-     {:mariaex, "~> 0.4.1", optional: true},
+     {:postgrex, ">= 0.9.1", optional: true},
+     {:mariaex, ">= 0.4.1", optional: true},
      {:poison, "~> 1.0", optional: true},
      {:plug, "~> 1.0", only: :test},
      {:ex_doc, "~> 0.7", only: :docs},
